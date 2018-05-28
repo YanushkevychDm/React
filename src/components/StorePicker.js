1d93daf13c3b404b06275d7cb8b9c8ab79947250
Dmytro Yanushkevych <onswarm@gmail.com>
2018-05-28 18:44:30 +0300

    add form and button into StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { format } from 'url';
 
 class StorePicker extends React.Component {
   render () {
-    return <p>I'm the Store Picker!</p>
+    return (
+      <React.Fragment>
+        {/* comment */}
+        <form action="" className="store-selector"> 
+          <h2>Please enter a store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store &rarr; </button>
+        </form>
+        
+      </React.Fragment>
+    )
+
   }
 }
 
