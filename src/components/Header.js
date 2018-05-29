329df495d55999d3fdbe0f184c6ba146ac2ae555
Dmytro Yanushkevych <onswarm@gmail.com>
2018-05-29 19:38:56 +0300

    add 8 fish cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Header = props => (
   <header className="top">
-    <h1>Catch 
+    <h1>
+      Catch
       <span className="ofThe">
-        <span className="of">of</span>
-        <span className="the">the</span>
+        <span className="of">Of</span>
+        <span className="the">The</span>
       </span>
-      Day</h1>
+      Day
+    </h1>
     <h3 className="tagline">
       <span>{props.tagline}</span>
     </h3>
-  </header>    
+  </header>
 );
 
 export default Header;
