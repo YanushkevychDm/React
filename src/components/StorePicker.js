20abad06fab14d89b2610fb28abd657f5f3e791a
Dmytro Yanushkevych <onswarm@gmail.com>
2018-05-29 06:36:02 +0300

    add routing between two pages - StorePicker and menu

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { format } from 'url';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render () {
+    console.log(this);
     return (
-      <React.Fragment>
-        {/* comment */}
-        <form action="" className="store-selector"> 
+        <form className="store-selector" onSubmit={this.goToStore}> 
           <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input 
+            type="text" 
+            ref={this.myInput}
+            required 
+            placeholder="Store Name" 
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store &rarr; </button>
         </form>
-        
-      </React.Fragment>
     )
 
   }
